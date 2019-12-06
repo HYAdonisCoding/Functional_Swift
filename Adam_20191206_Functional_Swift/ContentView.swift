e6d3d6afaffe39d18dff8bf7aeef2a135e0e4f86
曹弘扬 <296786475@qq.com>
2019-12-06 08:31:54 +0800

    Initial Commit

---
+++ Adam_20191206_Functional_Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Adam_20191206_Functional_Swift
+//
+//  Created by Adonis_HongYang on 2019/12/6.
+//  Copyright © 2019 Adonis_HongYang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
